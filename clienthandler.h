fe16c72b263b911bc773415b76d6d9e0161b6f76
robertorabei <robertorabei2005@gmail.com>
2024-12-11 19:26:43 +0100

    Update clienthandler.h

--- clienthandler.h
+++ clienthandler.h
@@ -3,11 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/select.h>
 #include <errno.h>
+
+#include "parametres.h"
 
 #define MAX_CLIENTS 1000
 #define PSEUDO_MAX_LENGTH 30
@@ -16,6 +19,8 @@
 typedef struct {
     int sockfd;                 // Descripteur de socket du client
     char pseudo[PSEUDO_MAX_LENGTH]; // Pseudonyme du client
+    bool isBot;                 
+    bool isManuel;
 } Client;
 
 // Liste globale des clients et compteur
@@ -28,7 +33,7 @@ extern int clientCount;
  * @param sockfd Le descripteur de socket du client.
  * @param pseudo Le pseudonyme du client.
  */
-void addClient(int sockfd, const char *pseudo);
+void addClient(int sockfd, DataClient* data);
 
 /**
  * Supprime un client de la liste des clients connectés.
