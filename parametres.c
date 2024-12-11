3ab5f786a948ab789a36fa9b5d2079cb14939bd5
robertorabei <robertorabei2005@gmail.com>
2024-12-11 19:25:37 +0100

    Update parametres.c

--- parametres.c
+++ parametres.c
@@ -5,48 +5,49 @@
 
 #include "parametres.h"
 
-static void VerifPseudo(int argc, char *argv[]){
-    if (argc < 2){
-        fputs("chat pseudo_utilisateur [--bot] [--manuel]\n", stderr);
+static void VerifPseudo(int argc, char *argv[]) {
+    if (argc < 2) {
+        fputs("Usage: chat pseudo_utilisateur [--bot] [--manuel]\n", stderr);
         exit(CODE_RETOUR_PARAMETRES_MANQUANTS);
     }
 
-    if (strlen(argv[1]) > LONGUEUR_PSEUDO_MAX){
+    if (strlen(argv[1]) > MAX_PSEUDO_DESTINATAIRE) {
         fputs("Pseudo doit avoir moins de 30 caractères.\n", stderr);
         exit(CODE_RETOUR_PSEUDO_TROP_LONG);
     }
 
     const char caracteresInterdits[] = { '/', '-', '[', ']' };
 
-    for (int i = 1; i < 3; ++i) {
-      for (unsigned int j = 0; j < sizeof(caracteresInterdits) / sizeof(*caracteresInterdits); ++j) {
-         if (strchr(argv[i], caracteresInterdits[j]) != NULL) {
+    for (unsigned int j = 0; j < sizeof(caracteresInterdits) / sizeof(*caracteresInterdits); ++j) {
+        if (strchr(argv[1], caracteresInterdits[j]) != NULL) {
             fprintf(stderr, "Le caractère '%c' n'est pas autorisé dans un pseudonyme.\n", caracteresInterdits[j]);
             exit(CODE_RETOUR_PSEUDO_CARACTERES_INVALIDES);
-         }
-      }
+        }
+    }
 
-      if (strcmp(argv[i], ".") == 0 || strcmp(argv[i], "..") == 0) {
-         fprintf(stderr, "Le pseudonyme '%s' n'est pas autorisé.\n", argv[i]);
-         exit(CODE_RETOUR_PSEUDO_CARACTERES_INVALIDES);
-      }
+    if (strcmp(argv[1], ".") == 0 || strcmp(argv[1], "..") == 0) {
+        fprintf(stderr, "Le pseudonyme '%s' n'est pas autorisé.\n", argv[1]);
+        exit(CODE_RETOUR_PSEUDO_CARACTERES_INVALIDES);
+    }
+}
+
+
+static void ExtraireData(int argc, char* argv[], DataClient* data) {
+   strncpy(data->pseudo, argv[1], MAX_PSEUDO_DESTINATAIRE - 1);
+   data->pseudo[MAX_PSEUDO_DESTINATAIRE - 1] = '\0';
+   data->isBot = false;
+   data->isManuel = false;
+   for (int i = 2; i < argc; ++i) {
+       if (strcmp(argv[i], "--bot") == 0) {
+           data->isBot = true;
+       } else if (strcmp(argv[i], "--manuel") == 0) {
+           data->isManuel = true;
+       }
    }
 }
 
-static void ExtraireFlags(int argc, char* argv[], OptionsProgramme* options) {
-   options->isBot = false;
-   options->isManuel = false;
-
-   for (int i = 3; i < argc; ++i) {
-      if (strcmp(argv[i], "--bot") == 0) {
-         options->isBot = true;
-      } else if (strcmp(argv[i], "--manuel") == 0) {
-         options->isManuel = true;
-      }
-   }
+void GererParametres(int argc, char* argv[], DataClient* data) {
+   VerifPseudo(argc, argv);
+   ExtraireData(argc, argv, data);
 }
 
-void GererParameteres(int argc, char* argv[], OptionsProgramme* options) {
-    VerifPseudo(argc, argv);
-    ExtraireFlags(argc, argv, options);
-}
