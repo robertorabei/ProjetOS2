ba0557bd413daab9ebe0825147d1de6ca37cf28b
Netrox <116916272+netroxxx@users.noreply.github.com>
2024-12-20 20:47:58 +0100

    Update signal.h

--- src/serveur/signal.h
+++ src/serveur/signal.h
@@ -4,29 +4,6 @@
 #include <stdbool.h>
 
 /**
- * Struct représentant la configuration d'un gestionnaire de signaux.
- * Elle est utilisée par la fonction sigaction pour configurer la gestion
- * des signaux.
- */
-struct sigaction{
-    // Ensemble de drapeaux controlant la manière de gérer les signaux
-    int sa_flags;
-
-
-    // Ensemble de signaux qui sont masqués pendant l'exécution du gestionnaire de signaux.
-    sigset_t sa_mask;
-
-    /**
-     * Sa_handler : un pointeur vers la fonction qui sera exécutée lorsqu'un
-     * signal est reçu. Dans ce cas, la fonction doit prendre un argument de
-     * type int (le numéro du signal) et ne rien retourner (void).
-     */
-    // Pointeur vers la fonction qui sera executé à la réception d'un signal
-    void (*sa_handler)(int);
-};
-
-
-/**
  * Réinitialise l'état de réception de SIGINT.
  */
 void ResetSigint(void);
